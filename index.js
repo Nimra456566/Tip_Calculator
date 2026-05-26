f0fab0a5587a6a789e25743db5b84f7cef9a8c4f
Nimra456566 <nimraarif785@gmail.com>
2026-05-26 10:58:10 +0500

    receving elements in variable

---
+++ index.js
@@ -0,0 +1,13 @@
+// Elements pakdo
+const billInput   = document.getElementById('bill');
+const tipInput    = document.getElementById('tip');
+const peopleInput = document.getElementById('people');
+const tipBtns     = document.querySelectorAll('.tip-btn');
+
+const outTip    = document.getElementById('out-tip');
+const outTotal  = document.getElementById('out-total');
+const outPerson = document.getElementById('out-person');
+
+const billError   = document.getElementById('bill-error');
+const tipError    = document.getElementById('tip-error');
+const peopleError = document.getElementById('people-error');
